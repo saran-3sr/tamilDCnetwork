f0681d4d0fc1d669a3bcf8dfc9418c663eb9314a
SaranPradeepJ <saranpradeep2003@gmail.com>
2022-10-12 21:30:17 +0530

    first commit

---
+++ exp6/client.py
@@ -0,0 +1,32 @@
+from concurrent.futures import thread
+from pydoc import cli
+from string import whitespace
+import socket
+
+
+import time
+import threading
+
+client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+client.connect(('localhost',80))
+
+def getmsg():
+  while True:
+    msg = client.recv(1000).decode('utf-8')
+    print("Server: ",msg)
+    time.sleep(0.5)
+
+def sendmsg():
+  while True:
+    msg = input("")
+    # print("\n")
+
+    client.sendall(msg.encode())
+    time.sleep(0.5)
+
+thread2 = threading.Thread(target=sendmsg)
+thread2.start()
+
+thread1 = threading.Thread(target=getmsg)
+thread1.start()
---
+++ fullDuplex/server.py
@@ -0,0 +1,41 @@
+from concurrent.futures import thread
+from string import whitespace
+import socket
+
+
+import time
+import threading
+
+server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+server.bind(('localhost',80))
+
+server.listen(1)
+
+print("Server Up and Running")
+
+conn,addr = server.accept()
+
+print("Connection Accepted",conn)
+
+def getmsg():
+  while True:
+    msg = conn.recv(1000).decode('utf-8')
+    print("Client: ",msg)
+    # print("\n")
+
+    time.sleep(0.5)
+
+def sendmsg():
+  while True:
+    msg = input("")
+    # print("\n")
+
+    conn.sendall(msg.encode())
+    time.sleep(0.5)
+
+thread1 = threading.Thread(target=getmsg)
+thread1.start()
+
+thread2 = threading.Thread(target=sendmsg)
+thread2.start()
